67327e8941cea46f834db0b374f76c53740bfdd7
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-08-11 22:54:03 +0500

    made some updates in starting design of my starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <header>
+        <h3>Name of the header goes here</h3>
+      </header>
+
+      <main>
+        <div className="app-div-dark">
+          <p>main Goes Here</p>
+        </div>
+      </main>
+
+      <footer>
+        <p>&copy; 2023 Khuram Shahzad : All right reserved.</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
